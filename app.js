81a199b3057fed2b82d232df950ff36f3bcfd47c
Emily Sellers <emilymelindasellers@gmail.com>
2022-09-20 09:33:51 -0700

    display scoreboard and message from state

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 
 /* Get DOM Elements */
+// const scoreboard = document.getElementById('scoreboard');
+const messageSection = document.getElementById('message-section');
+const scoreboardSection = document.getElementById('scoreboard-section');
 
 /* State */
+let message = 'You hit Daisy and did 4 in damage. Bonzo hit you and did 1 in damage.';
+let scoreboard = 'You have defeated 0 goblin(s).';
 
 /* Events */
 
 /* Display Functions */
+function displayMessage() {
+    messageSection.textContent = message;
+}
+
+function displayScoreboard() {
+    scoreboardSection.textContent = scoreboard;
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+displayMessage();
+displayScoreboard();
